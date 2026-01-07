a5290e426b790db8e83fc910d6d14aea5b91dc32
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-07 23:54:58 +0000

    components: ourStory initial setup and linked to homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import OurStory from "@/components/layout/OurStory";
 import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
     <>
-      <Footer/>
+      <OurStory />
+      <Footer />
     </>
   );
 }
---
+++ src/components/layout/OurStory.jsx
@@ -0,0 +1,7 @@
+
+
+export default function OurStory() {
+    return (
+        <div className="ourStory__container"></div>
+    )
+}
