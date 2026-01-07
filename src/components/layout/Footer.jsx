d4ef7e20e5c9303bb29d23802ca355601f599bcd
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-07 23:08:04 +0000

    components: FooterLocation html added to Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,13 +1,15 @@
 import FooterHeading from "./FooterHeading";
 import FooterContact from "./FooterContact";
+import FooterLocation from "./FooterLocation";
 import FooterLinks from "./FooterLinks";
 
 export default function Footer() {
     return (
         <footer className="footer__container">
-            <FooterHeading/>
-            <FooterContact/>
-            <FooterLinks/>
+            <FooterHeading />
+            <FooterContact />
+            <FooterLocation />
+            <FooterLinks />
         </footer>
     )
 }
---
+++ src/components/layout/FooterLocation.jsx
@@ -0,0 +1,13 @@
+
+
+export default function FooterLocation() {
+    return (
+        <section className="footerLocation__container">
+            <h3 className="footerLocation__location">
+                500 Terry Francine Street, 6th Floor,
+                <br/>
+                San Francisco, CA 94158
+            </h3>
+        </section>
+    )
+}
