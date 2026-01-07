51d88ce0821758ab099c62a3b257031a22565754
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-07 21:44:22 +0000

    src: footer initial setup and linked to homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Footer from "@/components/layout/Footer";
 
 export default function Home() {
   return (
-    <div>
-      {/*  */}
-    </div>
+    <>
+      <Footer/>
+    </>
   );
 }
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,1 +1,5 @@
-// 
+export default function Footer() {
+    return (
+        <footer className="footer__container"></footer>
+    )
+}
