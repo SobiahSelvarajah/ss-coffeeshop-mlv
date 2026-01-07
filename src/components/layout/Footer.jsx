295d6c4ae34e0b9934221b58743fdc122df1c85c
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-07 23:02:20 +0000

    components: FooterContact html added to Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,12 @@
 import FooterHeading from "./FooterHeading";
+import FooterContact from "./FooterContact";
 import FooterLinks from "./FooterLinks";
 
 export default function Footer() {
     return (
         <footer className="footer__container">
             <FooterHeading/>
+            <FooterContact/>
             <FooterLinks/>
         </footer>
     )
---
+++ src/components/layout/FooterContact.jsx
@@ -0,0 +1,14 @@
+
+
+export default function FooterContact() {
+    return (
+        <section className="footerContact__container">
+            <h3 className="footerContact__phone">
+                123-456-7890
+            </h3>
+            <h3 className="footerContact__email">
+                info@mlv.com
+            </h3>
+        </section>
+    )
+}
