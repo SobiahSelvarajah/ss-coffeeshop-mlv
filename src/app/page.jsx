18886f02870b0b26d36615cb6198065c6e42fdaf
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-08 23:22:47 +0000

    src:  hero setup and added to homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
+import Hero from "@/sections/Hero";
 import BestSellers from "@/sections/BestSellers";
 import OurStory from "@/sections/OurStory";
 
 export default function Home() {
   return (
     <>
+      <Hero />
       <BestSellers />
       <OurStory />
     </>
