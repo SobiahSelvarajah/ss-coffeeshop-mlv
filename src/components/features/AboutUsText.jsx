13d95f36551212ec6d315b7dc14efb891b2e2671
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-08 16:00:53 +0000

    src: aboutUsText html added to ourStory section

---
+++ src/components/features/AboutUsText.jsx
@@ -0,0 +1,16 @@
+
+
+export default function AboutUsText() {
+    return (
+        <p className="aboutUsText">
+            At My Little Valentine, we 
+            pride ourselves on blending 
+            classic elegance with 
+            traditional coffee preparation.
+            Our ingredients are sourced 
+            directly from trusted suppliers,
+            ensuring quality and authenticity
+            in every cup served.
+        </p>
+    )
+}
--- src/sections/OurStory.jsx
+++ src/sections/OurStory.jsx
@@ -1,10 +1,13 @@
 import OurStoryHeading from "@/components/features/OurStoryHeading";
+import AboutUsHeading from "@/components/features/AboutUsHeading";
+import AboutUsText from "@/components/features/AboutUsText";
 
 export default function OurStory() {
     return (
         <section className="ourStory__container">
             <OurStoryHeading />
-            
+            <AboutUsHeading /> 
+            <AboutUsText />
         </section>
     )
 }
