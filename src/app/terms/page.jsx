03d00193f8077bc6188592d40c5c3daaa67dac24
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-09 23:25:01 +0000

    app: terms page setup

---
+++ src/app/terms/page.jsx
@@ -0,0 +1,13 @@
+
+
+export const metadata = {
+    title: "Terms and Conditions",
+};
+
+export default function TermsPage() {
+    return (
+        <section className="termsPage__container">
+            {/*  */}
+        </section>
+    )
+}
