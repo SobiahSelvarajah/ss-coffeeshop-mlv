11965569bdfc76b855c79429e1fbb6af28e14459
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-10 21:47:39 +0000

    app: onlineOrder page setup

---
+++ src/app/online-order/page.jsx
@@ -0,0 +1,11 @@
+
+
+export const metadata = {
+    title: "Online Ordering",
+};
+
+export default function OnlineOrder() {
+    return (
+        <div className="onlineOrder__container"></div>
+    )
+}
