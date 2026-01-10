68bedc9e7cd38044045062e414385d1ebac3cca3
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-10 21:14:29 +0000

    app: myOrders page setup

---
+++ src/app/my-orders/page.jsx
@@ -0,0 +1,11 @@
+
+
+export const metadata = {
+    title: "My Orders",
+};
+
+export default function MyOrdersPage() {
+    return (
+        <div className="myOrdersPage__container"></div>
+    )
+}
