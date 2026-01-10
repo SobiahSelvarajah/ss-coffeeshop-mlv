82d3f17e7f9abe61c9b5f03fd84d69582c44d621
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2026-01-10 20:52:29 +0000

    app: reservation page setup

---
+++ src/app/reservation/page.jsx
@@ -0,0 +1,13 @@
+import ReservationHeading from "@/components/features/ReservationHeading";
+
+export const metadata = {
+    title: "Reservation",
+};
+
+export default function ReservationPage() {
+    return (
+        <div className="reservationPage__container">
+            <ReservationHeading />
+        </div>
+    )
+}
--- src/app/terms/page.jsx
+++ src/app/terms/page.jsx
@@ -14,4 +14,4 @@ export default function TermsPage() {
             <TermsBasics />
         </div>
     )
-}
+};
